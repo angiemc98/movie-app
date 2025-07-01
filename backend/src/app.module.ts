eaae62431eb2649f8d094cef23ab57192905b2a8
angiemc98 <molinanatalia@hotmail.com>
2025-06-30 20:03:28 -0500

    Backend nest, instalación de dependecias principales y conexión a DB

---
+++ backend/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'movies_db',
+      synchronize: true, // solo para desarrollo
+    }),
+  ],
+})
+export class AppModule {}
